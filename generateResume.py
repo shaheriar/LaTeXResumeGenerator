3010211550e3a77fa1a734d05217906da9d558b7
Shaheriar Malik <shaheriarm@gmail.com>
2024-03-08 11:15:58 -0700

    The user can now change the order of the sections

--- generateResume.py
+++ generateResume.py
@@ -1,11 +1,9 @@
 import json
 import os
 
-def generate_latex_resume(data, type):
-
-###### Header ######
-    
-    latex_resume = r'''
+#### Header ####
+def Header(data):
+    return r'''
 %%!TEX program = lualatex
 
 \documentclass[]{../mcdowellcv}
@@ -22,72 +20,74 @@ def generate_latex_resume(data, type):
 \makeheader
 ''' % (data["name"], data["email"], data["phone"], data["location"], data["linkedin"], data["linkedin"].split("www.")[1], data["github"], data["github"].split("www.")[1], data["website"], data["website"].split("https://")[1])
 
-###### Education ######
-
-    latex_resume += r'''
+#### Education ####
+def Education(data):
+    resumeStr = r'''
 \begin{cvsection}{Education}
 '''
     for education in data["Education"]:
-        latex_resume += r'''
+        resumeStr += r'''
     \begin{cvsubsection}{\mbox {%s}}{}{%s -- %s}
         %s in %s - GPA: %s
     \end{cvsubsection}
     ''' % (education["university"], education["from"], education["to"], education["degree"], education["major"], education["gpa"])
-
-###### Experience ######
-
-    latex_resume += r'''
+    resumeStr += r''' 
 \end{cvsection}
+'''
+    return resumeStr
 
+#### Experience ####
+def Experience(data, type):
+    resumeStr = r'''
 \begin{cvsection}{Experience}
 ''' 
-
     for experience in data["Experience"]:
         if type in experience["tags"]:
-            latex_resume += r'''
-        \begin{cvsubsection}{%s}{%s}{%s -- %s}
-            %s
-            \vspace{2.5mm}
-            \begin{itemize}
+            resumeStr += r'''
+    \begin{cvsubsection}{%s}{%s}{%s -- %s}
+        %s
+        \vspace{2.5mm}
+        \begin{itemize}
     ''' % (experience["title"], experience["company"], experience["from"], experience["to"], experience["location"])
 
             for desc in experience["description"]:
-                latex_resume += r'''            \item %s
+                resumeStr += r'''        \item %s
     ''' % desc
 
-        latex_resume += r'''        \end{itemize}
+            resumeStr += r'''    \end{itemize}
     \end{cvsubsection}
-'''
-
-###### Projects ######
-
-    latex_resume += r'''
+    '''
+    resumeStr += r'''
 \end{cvsection}
-
-\begin{cvsection}{Selected Projects}
 '''
+    return resumeStr
 
+#### Projects ####
+def Projects(data, type):
+    resumeStr = r'''
+\begin{cvsection}{Projects}
+'''
     for project in data["Projects"]:
         if type in project["tags"]:
-            latex_resume += r'''
-        \begin{cvsubsection}{}{}{}
-            \begin{itemize}
-                \setlength\itemsep{3pt}
-                \item\textbf{\href{%s}{%s}} \\
-                %s
-            \end{itemize}
-        \end{cvsubsection}
+            resumeStr += r'''
+    \begin{cvsubsection}{}{}{}
+        \begin{itemize}
+            \setlength\itemsep{3pt}
+            \item\textbf{\href{%s}{%s}} \\
+            %s
+        \end{itemize}
+    \end{cvsubsection}
     ''' % (project["link"], project["title"], project["description"])
-            
-###### Skills ######
 
-    latex_resume += r'''
+    resumeStr += r'''
 \end{cvsection}
-
-\begin{cvsection}{Skills}
 '''
+    return resumeStr
 
-    latex_resume += r'''
+#### Skills ####
+def Skills(data, type):
+    resumeStr = r'''
+\begin{cvsection}{Skills}
     \begin{cvsubsection}{}{}{}
         \begin{itemize}
             \item \textbf{Programming Languages:} %s
@@ -95,30 +95,36 @@ def generate_latex_resume(data, type):
             \item \textbf{Technologies:} %s
         \end{itemize}
     \end{cvsubsection}
-''' % (", ".join(data["Skills"]["ProgrammingLanguages"]), ", ".join(data["Skills"]["Software"][type]), ", ".join(data["Skills"]["Technologies"][type]))
-
-    latex_resume += r'''
 \end{cvsection}
+''' % (", ".join(data["Skills"]["ProgrammingLanguages"]), ", ".join(data["Skills"]["Software"][type]), ", ".join(data["Skills"]["Technologies"][type]))
+    return resumeStr
 
-\end{document}
-'''
-    return latex_resume
+#### End ####
+def End():
+    return r'''
+\end{document}'''
 
-###### FUNCTION END ######
+def generateResume(resume, data, type):
+    
+    # Change the order of the sections to change the order of the resume
+    resume += Experience(data, type)
+    resume += Education(data)
+    resume += Projects(data, type)
+    resume += Skills(data, type)
+
+    return resume
 
 def main():
     with open("configs/data.json", "r") as file:
         data = json.load(file)
 
-    types = data['Tags']
-    
-    for type in types:
-        latex_resume = generate_latex_resume(data, type.lower())
+    for type in data['Tags']:
+        latex_resume = generateResume(Header(data), data, type.lower())
         filepath = "GeneratedTexFiles/"+ type.capitalize() + "Resume.tex"
         with open(filepath, "w") as file:
-            file.write(latex_resume)
+            file.write(latex_resume + End())
         os.system("lualatex -interaction=nonstopmode -output-directory=GeneratedPDFs " + filepath)
-        os.system("del GeneratedPDFs\*.log GeneratedPDFs\*.aux GeneratedPDFs\*.out")
+        os.system("del GeneratedPDFs\\*.log GeneratedPDFs\*.aux GeneratedPDFs\*.out")
 
 if __name__ == "__main__":
     main()
